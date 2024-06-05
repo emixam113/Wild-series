c77754b1082ca5d0bf9a00545c984781da36f6b5
emixam113 <98613290+emixam113@users.noreply.github.com>
2024-06-05 18:11:31 +0200

    intial commit

--- server/index.js
+++ server/index.js
@@ -9,14 +9,20 @@ require("./database/client").checkConnection();
 // Import the Express application from app/config.js
 const app = require("./app/config");
 
+
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
 // Start the server and listen on the specified port
 app
+
+.post("/", (req, res) => {
+  res.send('Welcome to Wild Serie')
+})
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
   })
+  
   .on("error", (err) => {
     console.error("Error:", err.message);
   });
