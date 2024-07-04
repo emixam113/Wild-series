adaa688137e575f882d919a736dc973f85ee7a84
emixam113 <98613290+emixam113@users.noreply.github.com>
2024-07-04 12:33:58 +0200

    server is up

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -4,9 +4,27 @@ create table user (
   password varchar(255) not null
 );
 
-create table item (
+CREATE TABLE item (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null,
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+create table category(
+  id INT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL
+);
+
+
+CREATE TABLE program (
+    id INT PRIMARY KEY,
+    title varchar(100), 
+    synopsis TEXT NOT NULL, 
+    poster  TEXT NOT NULL, 
+    country VARCHAR(150), 
+    year INT,
+    category_id  INT, 
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
